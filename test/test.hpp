0e96cb32f8e5b31ce61bceb500a4f0e1e6567f72
Tuukka Norri <tsnorri@iki.fi>
2025-05-16 15:55:55 +0300

    Add macro for testing base case with GoogleTest
    – I noticed that RapidCheck prefers zero-sized input in some cases. To solve the issue, I added a macro that uses TEST for a base case (i.e. zero-sized value) and RC_GTEST_PROP otherwise.

--- test/test.hpp
+++ test/test.hpp
@@ -10,10 +10,49 @@
 #include <gtest/gtest.h>		// IWYU pragma: export
 #include <rapidcheck/gtest.h>	// IWYU pragma: export
 
+#if defined(SF_REPORT_TESTED_VALUE_DISTRIBUTION) && SF_REPORT_TESTED_VALUE_DISTRIBUTION
+#	define SF_RC_TAG(...) RC_TAG(__VA_ARGS__)
+#else
+#	define SF_RC_TAG(...)
+#endif
+
 // GoogleTest seems to require a fixture for each type-paramterised test,
 // so we have a macro to help with that.
 #define SF_RC_TEMPLATE_TEST(FIXTURE_PREFIX, TEST, TEST_PARAMS, ...) \
-	template <typename> struct FIXTURE_PREFIX##_fixture : public testing::Test{}; \
+	template <typename> struct FIXTURE_PREFIX##_rc_fixture : public testing::Test{}; \
 	typedef ::testing::Types <__VA_ARGS__> FIXTURE_PREFIX##_test_types; \
-	TYPED_TEST_SUITE(FIXTURE_PREFIX##_fixture, FIXTURE_PREFIX##_test_types); \
-	RC_GTEST_TYPED_FIXTURE_PROP(FIXTURE_PREFIX##_fixture, TEST, TEST_PARAMS)
+	TYPED_TEST_SUITE(FIXTURE_PREFIX##_rc_fixture, FIXTURE_PREFIX##_test_types); \
+	RC_GTEST_TYPED_FIXTURE_PROP(FIXTURE_PREFIX##_rc_fixture, TEST, TEST_PARAMS)
+
+// RapidCheck seems to repeatedly test with empty values in some cases.
+// We would like to do so but only once, so we use GoogleTest for the empty case.
+// Since we are not in RapidCheck's test case, we would like to conditionally use
+// GoogleTest's assertions.
+#define SF_ASSERT(CONDITION) (([&](){ if constexpr (t_uses_rapidcheck) RC_ASSERT(CONDITION) else ASSERT_TRUE(CONDITION))())
+
+// Mainly for checking if empty parentheses are passed in place of a macro parameter.
+// We could detect this with __VA_OPT__ but since it is fairly new, we use the GCC extension
+// which causes the comma in , ##__VA_ARGS__ to be deleted if __VA_ARGS__ is empty.
+#define SF_NARGS_(_0, _16, _15, _14, _13, _12, _11, _10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
+#define SF_NARGS(...) SF_NARGS_(0, ##__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+
+// We also provide this helpful macro for instantiating both the base case and the RC test.
+#define SF_RC_TEST_WITH_BASE_CASE(SUITE_NAME, TEST_NAME, BASE_CASE_PARAM, ARBITRARY_PARAM, TPL_PARAM, TAGS) \
+template <bool> void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl(TPL_PARAM); /* Fwd. */ \
+_Pragma("GCC diagnostic push") \
+_Pragma("GCC diagnostic ignored \"-Wunused-parameter\"") /* Ignore -Wunused-parameter in case SF_RC_TAG is a no-op. */ \
+inline void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _tag(TPL_PARAM) { /* Tag here since we don't know the variable name in TPL_PARAM. */ \
+	if (SF_NARGS TAGS) { \
+		SF_RC_TAG TAGS; \
+	} \
+} \
+_Pragma("GCC diagnostic pop") \
+TEST(SUITE_NAME, TEST_NAME ## BaseCase) { /* GoogleTest test */ \
+	BASE_CASE_PARAM val{}; \
+	test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl <false>(val); \
+} \
+RC_GTEST_PROP(SUITE_NAME, TEST_NAME, (ARBITRARY_PARAM val)) { /* RapidCheck test */ \
+	test_ ## SUITE_NAME ## _ ## TEST_NAME ## _tag(val); \
+	test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl <true>(val); \
+} \
+template <bool t_uses_rapidcheck> void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl(TPL_PARAM) /* Test function body follows. */
