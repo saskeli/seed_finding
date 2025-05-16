28c5daf2001b2dc93de0ffc84027d95dcb14bf9c
Tuukka Norri <tsnorri@iki.fi>
2025-05-16 16:09:13 +0300

    De-templatise test function in SF_RC_TEST_WITH_BASE_CASE

--- test/test.hpp
+++ test/test.hpp
@@ -28,7 +28,7 @@
 // We would like to do so but only once, so we use GoogleTest for the empty case.
 // Since we are not in RapidCheck's test case, we would like to conditionally use
 // GoogleTest's assertions.
-#define SF_ASSERT(CONDITION) (([&](){ if constexpr (t_uses_rapidcheck) RC_ASSERT(CONDITION) else ASSERT_TRUE(CONDITION))())
+#define SF_ASSERT(CONDITION) (([&](){ if (test_uses_rapidcheck) RC_ASSERT(CONDITION) else ASSERT_TRUE(CONDITION))())
 
 // Mainly for checking if empty parentheses are passed in place of a macro parameter.
 // We could detect this with __VA_OPT__ but since it is fairly new, we use the GCC extension
@@ -37,11 +37,11 @@
 #define SF_NARGS(...) SF_NARGS_(0, ##__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
 // We also provide this helpful macro for instantiating both the base case and the RC test.
-#define SF_RC_TEST_WITH_BASE_CASE(SUITE_NAME, TEST_NAME, BASE_CASE_PARAM, ARBITRARY_PARAM, TPL_PARAM, TAGS) \
-template <bool> void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl(TPL_PARAM); /* Fwd. */ \
+#define SF_RC_TEST_WITH_BASE_CASE(SUITE_NAME, TEST_NAME, BASE_CASE_PARAM, ARBITRARY_PARAM, FN_PARAM, TAGS) \
+void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl(FN_PARAM, bool); /* Fwd. */ \
 _Pragma("GCC diagnostic push") \
 _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"") /* Ignore -Wunused-parameter in case SF_RC_TAG is a no-op. */ \
-inline void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _tag(TPL_PARAM) { /* Tag here since we don't know the variable name in TPL_PARAM. */ \
+inline void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _tag(FN_PARAM) { /* Tag here since we don't know the variable name in FN_PARAM. */ \
 	if (SF_NARGS TAGS) { \
 		SF_RC_TAG TAGS; \
 	} \
@@ -49,10 +49,10 @@ inline void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _tag(TPL_PARAM) { /* Tag he
 _Pragma("GCC diagnostic pop") \
 TEST(SUITE_NAME, TEST_NAME ## BaseCase) { /* GoogleTest test */ \
 	BASE_CASE_PARAM val{}; \
-	test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl <false>(val); \
+	test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl(val, false); \
 } \
 RC_GTEST_PROP(SUITE_NAME, TEST_NAME, (ARBITRARY_PARAM val)) { /* RapidCheck test */ \
 	test_ ## SUITE_NAME ## _ ## TEST_NAME ## _tag(val); \
-	test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl <true>(val); \
+	test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl(val, true); \
 } \
-template <bool t_uses_rapidcheck> void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl(TPL_PARAM) /* Test function body follows. */
+void test_ ## SUITE_NAME ## _ ## TEST_NAME ## _impl(FN_PARAM, [[maybe_unused]] bool const test_uses_rapidcheck) /* Test function body follows. */
