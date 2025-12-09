e2771b85006a9016fc9466292f21b89c387b3f0a
Tuukka Norri <tsnorri@iki.fi>
2025-12-09 14:13:51 +0200

    Add packed_read::unpack(), packed_read_vector, print_read() for packed_read

--- include/pack_characters.hpp
+++ include/pack_characters.hpp
@@ -15,5 +15,18 @@ typedef std::vector<std::uint64_t> packed_word_vector;
                                             std::uint64_t dst_pos);
 void unpack_characters(std::vector<std::uint64_t> const& src,
                        std::uint64_t length, std::string& dst);
+
+
+struct packed_read {
+  packed_word_vector packed_characters;
+  std::uint64_t length{};
+
+  void unpack(std::string &dst) const { unpack_characters(packed_characters, length, dst); }
+};
+
+typedef std::vector <packed_read> packed_read_vector;
+
 void print_read(std::span<std::uint64_t const> span, std::uint64_t len);
+inline void print_read(packed_read const &read) { print_read(read.packed_characters, read.length); }
+
 }  // namespace sf
