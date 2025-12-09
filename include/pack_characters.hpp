18174b51fe17ce47dee5ca5649de36082d1adb31
Tuukka Norri <tsnorri@iki.fi>
2025-12-09 15:21:46 +0200

    Move packed_read to its own header, improve encapsulation

--- include/pack_characters.hpp
+++ include/pack_characters.hpp
@@ -16,17 +16,5 @@ typedef std::vector<std::uint64_t> packed_word_vector;
 void unpack_characters(std::vector<std::uint64_t> const& src,
                        std::uint64_t length, std::string& dst);
 
-
-struct packed_read {
-  packed_word_vector packed_characters;
-  std::uint64_t length{};
-
-  void unpack(std::string &dst) const { unpack_characters(packed_characters, length, dst); }
-};
-
-typedef std::vector <packed_read> packed_read_vector;
-
 void print_read(std::span<std::uint64_t const> span, std::uint64_t len);
-inline void print_read(packed_read const &read) { print_read(read.packed_characters, read.length); }
-
 }  // namespace sf
